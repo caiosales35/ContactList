7284bdd0b616dc022b4666aab53c0d31c88e4828
Caio Cesar de Morais Sales <caiosales35@gmail.com>
2021-01-08 00:26:11 -0300

    User model - generate model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_08_024757) do
+ActiveRecord::Schema.define(version: 2021_01_08_031546) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
